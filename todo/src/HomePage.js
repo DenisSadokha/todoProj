f7521162bce57f26ea481b867289c47dbcc5d938
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-23 10:50:54 +0700

    Update HomePage.js

--- todo/src/HomePage.js
+++ todo/src/HomePage.js
@@ -1,22 +1,29 @@
 "use strict";
+import {observer} from "mobx-react"
 import React, {Component} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Reg from './Reg';
 import Notes from './Notes';
 
+
 export default class HomePage extends Component{
     render(){
         return (
             <Router>
+
             <div>
-                <a href = "/notes">Заметки</a>
-                <a href = "/registr">Зарегистрироваться</a>                
+               <a href = "/notes">Заметки {"\n"}</a>
+                <a href = "/registr">Зарегистрироваться {"\n"}</a>                
+                <h1>
+                    its home page!
+                </h1>
             </div>
             <Switch>
                  <Route exact path = "/notes" component ={Notes} ></Route>
                  <Route exact path = "/registr" component ={Reg} ></Route>
             </Switch>
             </Router>
+
         )
 
     }
