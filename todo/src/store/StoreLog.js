c30ff5d7c3af7a279e7e7b33d8f4034e52a8b3a3
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-23 10:46:43 +0700

    Update StoreLog.js

--- todo/src/store/StoreLog.js
+++ todo/src/store/StoreLog.js
@@ -1,19 +1,60 @@
-import {action, observable, decorate } from "mobx";
+import {action, observable, decorate, computed} from "mobx";
+import StoreNotes from "./StoreNotes"
+let form;
+const reqApi="http://localhost:3001/api/Users/login";
+  class StoreLog{
+      login;
+      pass;
+      mess;
+      isValid;
+      signIn(login, pass){
+          // this.mess="ошибка входа";
+          // this.login = login;
+          this.pass=pass;
+          form  = {
+            email: login,
+            password: pass
+          }
+        
+      this.makeRequest(form);
+    
+      }
+    
+     async makeRequest(form){
+       try{
+        const response=  await fetch(reqApi, {
+          method: 'POST',
+          body: JSON.stringify(form),
+          headers:  {
+           'Content-Type': 'application/json'
+         }
+      });
+      if (response.ok) {
+        this.isValid=true;
+        StoreNotes.setAuth(true);
+        this.mess='';
+        const json = await response.json();
+       console.log(json.userId)
+        console.log(json);
+       } else{
+        this.isValid=false;
+        this.mess="ошибка входа";
 
-class Store {
-    login;
-    pass;
-    passCheck;
-    onRegistr(login, password){
-        this.login=login;
+       }
 
-    }
+       } catch(error){
+        this.isValid=false;
+        this.mess="ошибка входа";
 
-}
- Store = decorate(Store, {
+       }
+      }
+      
+  }
+  StoreLog = decorate(StoreLog, {
     login: observable,
     pass: observable,
-    passCheck: observable,
-    onRegistr: action
+    mess: observable,
+    isValid: observable,
+    signIn: action
 })
-export default new Store();
+  export default new StoreLog();
