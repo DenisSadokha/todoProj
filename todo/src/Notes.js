19a7da1ec6b4cc9f7124f129dd11a3cfaca7940d
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-23 10:49:57 +0700

    Update Notes.js

--- todo/src/Notes.js
+++ todo/src/Notes.js
@@ -1,19 +1,45 @@
 "use strict"
 import React, {Component} from "react";
-export default class Notes extends Component{
+import Login from "./Login"
+import { Observer, observer } from "mobx-react";
 
+import Login1 from "./Reg"
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import store from "./store/StoreNotes"
+import storeLog from "./store/StoreLog"
+
+
+
+class Notes extends Component{
     render(){
-        let isValid = false;
+        console.log("NOTES")
+        console.log(store.auth)
 
-        if(isValid){
+
+        if(store.auth){
+            return (
+                <div>
+                    <h1>
+                        {storeLog.pass}
+                        
+                    </h1>
+                </div>
+
+            )
 
         } else{
             return(
+                <Router>
                 <div>
-                    <h1>
-                        регистрация
-                    </h1>
+                    <a href="/login">Войти</a>
                 </div>
+                <Switch>
+                    <Route exact path = "/login" component = {Login}></Route>
+                </Switch>
+                </Router>
+
+                
+
             )
         }
     }
@@ -21,3 +47,4 @@ export default class Notes extends Component{
     
 
 }
+export default observer(Notes);
